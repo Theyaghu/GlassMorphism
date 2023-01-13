0119eb66bc74ae6bd67cc48e78e63d3fbe750e30
Theyaghu <theyagarajants@gmail.com>
2023-01-14 02:13:31 +0530

    Initial Commit

---
+++ GlassmorphismDemo/GlassmorphismDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismDemoApp.swift
+//  GlassmorphismDemo
+//
+//  Created by THEYAGARAJAN T S on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
