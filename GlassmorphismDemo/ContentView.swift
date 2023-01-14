9bfcf50efedad751af5eef77fbe4d3f9ab6ae279
Theyaghu <theyagarajants@gmail.com>
2023-01-14 16:31:32 +0530

    till first text

--- GlassmorphismDemo/ContentView.swift
+++ GlassmorphismDemo/ContentView.swift
@@ -12,8 +12,22 @@ struct ContentView: View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.white, .white.opacity(0.0)]), startPoint: .top, endPoint: .bottom)
             
-            LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)).opacity(0.4), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).opacity(0.6) ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+            ZStack {
+                Color.white.opacity(0.35)
+                    .frame(width: 300, height: 400)
+                    .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0 , y: 10)
+                Text("Glass Morphism")
+                    .font(.system(size: 24, weight: .bold, design: .rounded))
+                    .foregroundColor(Color.black.opacity(0.8))
+                
+            }
+            
+            
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
