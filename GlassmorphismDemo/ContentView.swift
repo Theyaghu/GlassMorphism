a83e7faa58a1236baf1ba61c558cc1dc2489c84e
Theyaghu <theyagarajants@gmail.com>
2023-01-15 00:00:29 +0530

    Think Final

--- GlassmorphismDemo/ContentView.swift
+++ GlassmorphismDemo/ContentView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.white, .white.opacity(0.0)]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))]), startPoint: .top, endPoint: .bottom)
             
-            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)).opacity(0.4), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).opacity(0.6) ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)).opacity(0.6), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).opacity(0.4) ]), startPoint: .topLeading, endPoint: .bottomTrailing)
             
-            PopView()
+            ZStack {
+                Circle()
+                    .frame(width: 400, height: 400)
+                    .offset(x: 150, y: -200)
+                    .foregroundColor(Color.purple.opacity(0.4))
+                    .blur(radius: 8)
+                Circle()
+                    .frame(width: 300, height: 300)
+                    .offset(x: -100, y: -125)
+                    .foregroundColor(Color.green.opacity(0.4))
+                    .blur(radius: 8)
+                PopView()
+            }
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -29,7 +41,7 @@ struct ContentView_Previews: PreviewProvider {
 struct PopView: View {
     var body: some View {
         ZStack(alignment: .topLeading){
-            Color.white.opacity(0.35)
+            Color.white.opacity(0.5)
                 .frame(width: 300, height: 400)
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0 , y: 10)
